08d246437bb073e6b0b5cbfc73ca65080f4d3dfb
Orbittal123 <165017941+Orbittal123@users.noreply.github.com>
2024-05-25 11:38:57 +0530

    Update tcplistner.js

--- tcplistner.js
+++ tcplistner.js
@@ -79,7 +79,7 @@ const server = net.createServer((socket) => {
   });
 });
 
-const PORT = 4000;
+const PORT = 7080;
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
