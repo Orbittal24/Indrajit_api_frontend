b5bcee66472b16437eceb0a009fd045c9733addd
Orbittal123 <165017941+Orbittal123@users.noreply.github.com>
2024-06-25 11:17:40 +0530

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import DarkModeSwitcher from "./DarkModeSwitcher";
 import DropdownMessage from "./DropdownMessage";
-// import DropdownNotification from "./DropdownNotification";
-// import DropdownUser from "./DropdownUser";
+import DropdownNotification from "./DropdownNotification";
+import DropdownUser from "./DropdownUser";
 import Image from "next/image";
 
  const Header = ( 
- // props: {
-//   sidebarOpen: string | boolean | undefined;
-//   setSidebarOpen: (arg0: boolean) => void;
-// }
+props: {
+  sidebarOpen: string | boolean | undefined;
+  setSidebarOpen: (arg0: boolean) => void;
+}
 ) => {
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
@@ -135,13 +135,13 @@ import Image from "next/image";
             {/* <!-- Notification Menu Area --> */}
 
             {/* <!-- Chat Notification Area --> */}
-            <DropdownMessage />
+            {<DropdownMessage /> }
             {/* <!-- Chat Notification Area --> */}
           </ul>
 
           {/* <!-- User Area --> */}
-          {/* <DropdownUser /> */}
-          {/* <!-- User Area --> */}
+          {/* {<DropdownUser />} */}
+          {/* < User Area --> */}
         </div>
       </div>
     </header>
