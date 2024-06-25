8b1757dd10633512d237846ed2db469b682fc0ac
Orbittal123 <165017941+Orbittal123@users.noreply.github.com>
2024-06-25 09:45:42 +0530

    Update TableOne.tsx

--- src/components/Tables/TableOne.tsx
+++ src/components/Tables/TableOne.tsx
@@ -3,79 +3,80 @@ import Image from "next/image";
 
 const brandData: BRAND[] = [
   {
-    logo: "/images/brand/brand-01.svg",
-    name: "Google",
-    visitors: 3.5,
-    revenues: "5,768",
-    sales: 590,
-    conversion: 4.8,
+    weekly: "WEEKLY",
+    // name: "Google",
+    visitors: 30.011,
+    revenues: 40.1,
+    sales: 25.012,
+    conversion:35.00,
+    
   },
   {
-    logo: "/images/brand/brand-02.svg",
-    name: "Twitter",
-    visitors: 2.2,
-    revenues: "4,635",
-    sales: 467,
-    conversion: 4.3,
+    weekly: "MONTHALY",
+    // name: "Google",
+    visitors: 30.011,
+    revenues: 57.01,
+    sales: 25.012,
+    conversion:35.00,
   },
-  {
-    logo: "/images/brand/brand-03.svg",
-    name: "Github",
-    visitors: 2.1,
-    revenues: "4,290",
-    sales: 420,
-    conversion: 3.7,
-  },
-  {
-    logo: "/images/brand/brand-04.svg",
-    name: "Vimeo",
-    visitors: 1.5,
-    revenues: "3,580",
-    sales: 389,
-    conversion: 2.5,
-  },
-  {
-    logo: "/images/brand/brand-05.svg",
-    name: "Facebook",
-    visitors: 3.5,
-    revenues: "6,768",
-    sales: 390,
-    conversion: 4.2,
-  },
+  // {
+  //   logo: "/images/brand/brand-03.svg",
+  //   name: "Github",
+  //   visitors: 2.1,
+  //   revenues: "4,290",
+  //   sales: 420,
+  //   conversion: 3.7,
+  // },
+  // {
+  //   logo: "WEEKLY",
+  //   name: "Vimeo",
+  //   visitors: 1.5,
+  //   revenues: "3,580",
+  //   sales: 389,
+  //   conversion: 2.5,
+  // },
+  // {
+  //   logo: "/images/brand/brand-05.svg",
+  //   name: "Facebook",
+  //   visitors: 3.5,
+  //   revenues: "6,768",
+  //   sales: 390,
+  //   conversion: 4.2,
+  // },
 ];
 
 const TableOne = () => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        Top Channels
+        AVG CYCLE TIME OF WEEKLY & MONTHALY 
       </h4>
 
       <div className="flex flex-col">
         <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
           <div className="p-2.5 xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Source
+             period
             </h5>
           </div>
           <div className="p-2.5 text-center xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Visitors
+              VISION1
             </h5>
           </div>
           <div className="p-2.5 text-center xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Revenues
+            VISION2
             </h5>
           </div>
           <div className="hidden p-2.5 text-center sm:block xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Sales
+             WELDING
             </h5>
           </div>
           <div className="hidden p-2.5 text-center sm:block xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Conversion
+             FPCB
             </h5>
           </div>
         </div>
@@ -91,23 +92,25 @@ const TableOne = () => {
           >
             <div className="flex items-center gap-3 p-2.5 xl:p-5">
               <div className="flex-shrink-0">
-                <Image src={brand.logo} alt="Brand" width={48} height={48} />
-              </div>
               <p className="hidden text-black dark:text-white sm:block">
-                {brand.name}
+                {brand.weekly}
               </p>
+              </div>
+              {/* <p className="hidden text-black dark:text-white sm:block">
+                {brand.name}
+              </p> */}
             </div>
 
             <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-black dark:text-white">{brand.visitors}K</p>
+              <p className="text-black dark:text-white">{brand.visitors}%</p>
             </div>
 
             <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-meta-3">${brand.revenues}</p>
+              <p className="text-meta-3">{brand.revenues}%</p>
             </div>
 
             <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-black dark:text-white">{brand.sales}</p>
+              <p className="text-black dark:text-white">{brand.sales}%</p>
             </div>
 
             <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
@@ -121,3 +124,116 @@ const TableOne = () => {
 };
 
 export default TableOne;
+// "use client";
+
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+// const TableOne = () => {
+//   const [brandData, setBrandData] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('http://localhost:5501/api/CountAll');
+//         const data = response.data;
+
+//         const formattedData = [
+//           {
+//             period: 'WEEKLY',
+//             vision1: data.vision1weekavg || "-",
+//             vision2: data.vision2weekavg,
+//             welding: data.weldingweekavg,
+//             fpcb: data.fpcbweekavg,
+//           },
+//           {
+//             period: 'MONTHLY',
+//             vision1: data.vision1monthavg  || "-",
+//             vision2: data.vision2monthavg,
+//             welding: data.weldingmonthavg,
+//             fpcb: data.fpcbmonthavg,
+//           }
+//         ];
+
+//         setBrandData(formattedData);
+//       } catch (error) {
+//         console.error('Error fetching data:', error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+//       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
+//         AVG OF WEEKLY & MONTHLY CYCLE TIME
+//       </h4>
+
+//       <div className="flex flex-col">
+//         <div className="grid grid-cols-5 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
+//           <div className="p-2.5 xl:p-5">
+//             <h5 className="text-sm font-medium uppercase xsm:text-base">
+//               PERIOD
+//             </h5>
+//           </div>
+//           <div className="p-2.5 text-center xl:p-5">
+//             <h5 className="text-sm font-medium uppercase xsm:text-base">
+//               VISION1
+//             </h5>
+//           </div>
+//           <div className="p-2.5 text-center xl:p-5">
+//             <h5 className="text-sm font-medium uppercase xsm:text-base">
+//               VISION2
+//             </h5>
+//           </div>
+//           <div className="hidden p-2.5 text-center sm:block xl:p-5">
+//             <h5 className="text-sm font-medium uppercase xsm:text-base">
+//               WELDING
+//             </h5>
+//           </div>
+//           <div className="hidden p-2.5 text-center sm:block xl:p-5">
+//             <h5 className="text-sm font-medium uppercase xsm:text-base">
+//               FPCB
+//             </h5>
+//           </div>
+//         </div>
+
+//         {brandData.map((brand, key) => (
+//           <div
+//             className={`grid grid-cols-5 ${
+//               key === brandData.length - 1
+//                 ? ""
+//                 : "border-b border-stroke dark:border-strokedark"
+//             }`}
+//             key={key}
+//           >
+//             <div className="flex items-center gap-3 p-2.5 xl:p-5">
+//               <div className="flex-shrink-0">
+//                 <p className="text-black dark:text-white">{brand.period}</p>
+//               </div>
+//             </div>
+
+//             <div className="flex items-center justify-center p-2.5 xl:p-5">
+//               <p className="text-black dark:text-white">{brand.vision1}%</p>
+//             </div>
+
+//             <div className="flex items-center justify-center p-2.5 xl:p-5">
+//               <p className="text-meta-3">{brand.vision2}%</p>
+//             </div>
+
+//             <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+//               <p className="text-black dark:text-white">{brand.welding}%</p>
+//             </div>
+
+//             <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+//               <p className="text-meta-5">{brand.fpcb}%</p>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TableOne;
