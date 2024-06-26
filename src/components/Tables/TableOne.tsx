0406e26047336eceba1b368f19e37571b4736504
Orbittal123 <165017941+Orbittal123@users.noreply.github.com>
2024-06-26 10:23:07 +0530

    Update TableOne.tsx

--- src/components/Tables/TableOne.tsx
+++ src/components/Tables/TableOne.tsx
@@ -1,62 +1,57 @@
-import { BRAND } from "@/types/brand";
-import Image from "next/image";
+"use client";
 
-const brandData: BRAND[] = [
-  {
-    weekly: "WEEKLY",
-    // name: "Google",
-    visitors: 30.011,
-    revenues: 0,
-    sales: 0,
-    conversion:0,
-    
-  },
-  {
-    weekly: "MONTHALY",
-    // name: "Google",
-    visitors: 30.011,
-    revenues: 0,
-    sales: 0,
-    conversion:0,
-  },
-  // {
-  //   logo: "/images/brand/brand-03.svg",
-  //   name: "Github",
-  //   visitors: 30.011,
-  //   revenues: 0,
-  //   sales: 0,
-  //   conversion: 3.7,
-  // },
-  // {
-  //   logo: "WEEKLY",
-  //   name: "Vimeo",
-  //   visitors: 1.5,
-  //   revenues: "3,580",
-  //   sales: 389,
-  //   conversion: 2.5,
-  // },
-  // {
-  //   logo: "/images/brand/brand-05.svg",
-  //   name: "Facebook",
-  //   visitors: 3.5,
-  //   revenues: "6,768",
-  //   sales: 390,
-  //   conversion: 4.2,
-  // },
-];
+import { useEffect, useState } from 'react';
 
 const TableOne = () => {
+  const [brandData, setBrandData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5501/api/WeekMonthDiff');
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log("swamii", data);
+
+        const formattedData = [
+          {
+            period: 'WEEKLY',
+            vision1: data.v1_weekDiff_Avg ,
+            vision2: data.v2_weekDiff_Avg,
+            welding: data.welding_weekDiff_Avg,
+            fpcb: data.fpcb_Diff_weekAvg,
+          },
+          {
+            period: 'MONTHLY',
+            vision1: data.v1_monthDiff_Avg || "-",
+            vision2: data.v2_monthDiff_Avg,
+            welding: data.welding_monthDiff_Avg,
+            fpcb: data.fpcb_Diff_monthAvg,
+          }
+        ];
+
+        setBrandData(formattedData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        AVG CYCLE TIME OF WEEKLY & MONTHALY 
+        AVG OF WEEKLY & MONTHLY CYCLE TIME
       </h4>
 
       <div className="flex flex-col">
-        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
+        <div className="grid grid-cols-5 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
           <div className="p-2.5 xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-             period
+              PERIOD
             </h5>
           </div>
           <div className="p-2.5 text-center xl:p-5">
@@ -66,24 +61,24 @@ const TableOne = () => {
           </div>
           <div className="p-2.5 text-center xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-            VISION2
+              VISION2
             </h5>
           </div>
           <div className="hidden p-2.5 text-center sm:block xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-             WELDING
+              WELDING
             </h5>
           </div>
           <div className="hidden p-2.5 text-center sm:block xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-             FPCB
+              FPCB
             </h5>
           </div>
         </div>
 
         {brandData.map((brand, key) => (
           <div
-            className={`grid grid-cols-3 sm:grid-cols-5 ${
+            className={`grid grid-cols-5 ${
               key === brandData.length - 1
                 ? ""
                 : "border-b border-stroke dark:border-strokedark"
@@ -92,29 +87,24 @@ const TableOne = () => {
           >
             <div className="flex items-center gap-3 p-2.5 xl:p-5">
               <div className="flex-shrink-0">
-              <p className="hidden text-black dark:text-white sm:block">
-                {brand.weekly}
-              </p>
+                <p className="text-black dark:text-white">{brand.period}</p>
               </div>
-              {/* <p className="hidden text-black dark:text-white sm:block">
-                {brand.name}
-              </p> */}
             </div>
 
             <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-black dark:text-white">{brand.visitors}%</p>
+              <p className="text-black dark:text-white">{brand.vision1}%</p>
             </div>
 
             <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-meta-3">{brand.revenues}%</p>
+              <p className="text-meta-3">{brand.vision2}%</p>
             </div>
 
             <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-black dark:text-white">{brand.sales}%</p>
+              <p className="text-black dark:text-white">{brand.welding}%</p>
             </div>
 
             <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-meta-5">{brand.conversion}%</p>
+              <p className="text-meta-5">{brand.fpcb}%</p>
             </div>
           </div>
         ))}
@@ -124,116 +114,4 @@ const TableOne = () => {
 };
 
 export default TableOne;
-// "use client";
 
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
-
-// const TableOne = () => {
-//   const [brandData, setBrandData] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get('http://localhost:5501/api/CountAll');
-//         const data = response.data;
-
-//         const formattedData = [
-//           {
-//             period: 'WEEKLY',
-//             vision1: data.vision1weekavg || "-",
-//             vision2: data.vision2weekavg,
-//             welding: data.weldingweekavg,
-//             fpcb: data.fpcbweekavg,
-//           },
-//           {
-//             period: 'MONTHLY',
-//             vision1: data.vision1monthavg  || "-",
-//             vision2: data.vision2monthavg,
-//             welding: data.weldingmonthavg,
-//             fpcb: data.fpcbmonthavg,
-//           }
-//         ];
-
-//         setBrandData(formattedData);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
-//       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-//         AVG OF WEEKLY & MONTHLY CYCLE TIME
-//       </h4>
-
-//       <div className="flex flex-col">
-//         <div className="grid grid-cols-5 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
-//           <div className="p-2.5 xl:p-5">
-//             <h5 className="text-sm font-medium uppercase xsm:text-base">
-//               PERIOD
-//             </h5>
-//           </div>
-//           <div className="p-2.5 text-center xl:p-5">
-//             <h5 className="text-sm font-medium uppercase xsm:text-base">
-//               VISION1
-//             </h5>
-//           </div>
-//           <div className="p-2.5 text-center xl:p-5">
-//             <h5 className="text-sm font-medium uppercase xsm:text-base">
-//               VISION2
-//             </h5>
-//           </div>
-//           <div className="hidden p-2.5 text-center sm:block xl:p-5">
-//             <h5 className="text-sm font-medium uppercase xsm:text-base">
-//               WELDING
-//             </h5>
-//           </div>
-//           <div className="hidden p-2.5 text-center sm:block xl:p-5">
-//             <h5 className="text-sm font-medium uppercase xsm:text-base">
-//               FPCB
-//             </h5>
-//           </div>
-//         </div>
-
-//         {brandData.map((brand, key) => (
-//           <div
-//             className={`grid grid-cols-5 ${
-//               key === brandData.length - 1
-//                 ? ""
-//                 : "border-b border-stroke dark:border-strokedark"
-//             }`}
-//             key={key}
-//           >
-//             <div className="flex items-center gap-3 p-2.5 xl:p-5">
-//               <div className="flex-shrink-0">
-//                 <p className="text-black dark:text-white">{brand.period}</p>
-//               </div>
-//             </div>
-
-//             <div className="flex items-center justify-center p-2.5 xl:p-5">
-//               <p className="text-black dark:text-white">{brand.vision1}%</p>
-//             </div>
-
-//             <div className="flex items-center justify-center p-2.5 xl:p-5">
-//               <p className="text-meta-3">{brand.vision2}%</p>
-//             </div>
-
-//             <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-//               <p className="text-black dark:text-white">{brand.welding}%</p>
-//             </div>
-
-//             <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-//               <p className="text-meta-5">{brand.fpcb}%</p>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TableOne;
