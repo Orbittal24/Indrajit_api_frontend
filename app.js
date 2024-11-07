94fd528f4077898685b2f4ea1630c9339e1ca064
Orbittal123 <165017941+Orbittal123@users.noreply.github.com>
2024-11-07 17:19:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -455,8 +455,8 @@ async function processRFIDTagsSingle(tags, socket) {
       const result1 = await request.query(selectQuery);
       if (result1.recordset.module_barcode != '' && result1.recordset.RFID != '') {
         // Send success message to frontend for linking
-        broadcast({ message: 'Module Barcode and RFID linked successfully!' });
-        console.log("Module Barcode and RFID linked successfully!");
+        broadcast({ message: 'Module Barcode and RFID linked successfully!..' });
+        console.log("Module Barcode and RFID linked successfully!..");
 
         // Write the CycleStartConfirm tag to true for Vision1
         await writeCycleStartConfirm(tags.vision1.RFID, socket, true);
